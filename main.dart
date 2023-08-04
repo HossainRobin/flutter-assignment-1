43a01e9d5dc0411c2d8444238184edd423f7f2b1
HossainRobin <hellorobinhossain@gmail.com>
2023-08-04 19:00:32 +0600

    addition, subtraction, multiplication, division, modulus added

--- main.dart
+++ main.dart
@@ -1,1 +1,22 @@
-void main() {}
+void main() {
+  const a = 7;
+  const b = 3;
+
+  print("a = $a");
+  print("b = $b");
+
+  int add = a + b;
+  print("Addition: $a + $b = $add");
+
+  int sub = a - b;
+  print("Subtraction: $a - $b = $sub");
+
+  int multi = a * b;
+  print("Multiplication: $a * $b = $multi");
+
+  double division = a / b;
+  print("Division: $a / $b = $division");
+
+  dynamic modulus = a % b;
+  print("Modulus: $a % $b = $modulus");
+}
